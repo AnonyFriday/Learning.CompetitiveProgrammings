bde28df1de236829ac3306450ab9e290389f33ce
AnonyFriday <duykimvu75@gmail.com>
2025-09-15 01:23:52 +0700

    add: add valid anagram

--- Easy/5_String/09_ValidAnagram.cpp
+++ Easy/5_String/09_ValidAnagram.cpp
@@ -12,15 +12,37 @@
 // #include <set>
 // #include <stack>
 // #include <unordered_map>
-// #include <vector>
-//
+
 using namespace std;
 
 const double E = 1e-8;
 const double PI = acos(-1);
 
+bool isAnagram(string& s, string& t) {
+  int x = 0;
+
+  // using a ^ a = 0 bitwise operation
+
+  for (const int& a : s) {
+    x ^= a;
+  }
+
+  for (const int& b : t) {
+    x ^= b;
+  }
+
+  return x == 0;
+}
+
 int main() {
   ios_base::sync_with_stdio(0);
+
+  string s, t;
+
+  getline(std::cin, s);
+  getline(std::cin, t);
+
+  cout << "Is Anagram: " << isAnagram(s, t);
 
   return 0;
 }
