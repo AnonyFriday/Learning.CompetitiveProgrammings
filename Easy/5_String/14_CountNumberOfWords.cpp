8d3287f6a4d36416e0b6f414fb3b4f56e11272ae
AnonyFriday <duykimvu75@gmail.com>
2025-09-27 05:38:51 +0700

    feat(countNumberOfWords): add solution for special chars inclusive and exclusive

--- Easy/5_String/14_CountNumberOfWords.cpp
+++ Easy/5_String/14_CountNumberOfWords.cpp
@@ -19,7 +19,7 @@ using namespace std;
 const double E = 1e-8;
 const double PI = acos(-1);
 
-int getNumberOfWords(string &sentence) {
+int getNumberOfWordsWithoutSpecial(string &sentence) {
   if (sentence.size() == 0) return 0;
 
   // create a flag for marking a word
@@ -45,13 +45,40 @@ int getNumberOfWords(string &sentence) {
   return count;
 }
 
+int getNumberOfWordsWithSpecial(string &sentence) {
+  if (sentence.size() == 0) return 0;
+
+  // a flag to detach if current in word zone
+  // when the state changes, then the word is count to 1
+  bool isWordZone = false;
+  int count = 0;
+
+  // check if in space zone
+  for (const char &c : sentence) {
+    // if current is space, then continue and set the flag to false
+    if (isspace(c)) {
+      isWordZone = false;
+      continue;
+    } else {
+      // a character now, and just transition to a word
+      if (isWordZone == false) {
+        count++;
+        isWordZone = true;
+      }
+    }
+  }
+
+  return count++;
+}
+
 int main() {
   ios_base::sync_with_stdio(0);
 
   string s;
   getline(cin, s);
 
-  cout << getNumberOfWords(s);
+  cout << getNumberOfWordsWithoutSpecial(s) << endl;
+  cout << getNumberOfWordsWithSpecial(s);
 
   return 0;
 }
