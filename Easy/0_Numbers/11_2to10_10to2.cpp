8c208050e99800ce5baa5a4868553ba189217573
anonyfriday <duykimvu75@gmail.com>
2025-10-21 16:30:00 +0700

    feat(number): add convert bi string to dev

--- Easy/0_Numbers/11_2to10_10to2.cpp
+++ Easy/0_Numbers/11_2to10_10to2.cpp
@@ -10,9 +10,11 @@ using namespace std;
 unsigned long long int decToBi(unsigned long long int n) {
   unsigned long long int result = 0, p = 1;
 
+  // if number is binary, then get a digit buy % 2, not % 10
+
   while (n > 0) {
     int remainder = n % 2;
-    result = result + remainder * p;
+    result += remainder * p;  // _ _ _ _ 0, _ _ _ 0 1
     p *= 10;
     n /= 2;
   }
@@ -28,7 +30,7 @@ unsigned long long int biToDec(unsigned long long int n) {
 
   while (n > 0) {
     int digit = n % 10;
-    result = result + digit * p;
+    result += digit * p;
     p *= 2;
     n /= 10;
   }
@@ -39,15 +41,25 @@ unsigned long long int biToDec(unsigned long long int n) {
 /*
 
         Convert string binay to decimal
-
 */
 
-unsigned long long int biStringToDec(string n) {
+unsigned long long int biStringToDec(const string& n) {
   unsigned long long int result = 0, p = 1;
 
+  // get the right most bit, convert
+
+  // for (int i = n.size() - 1; i >= 0; i--) {
+  // int bit = n[i] - '0';
+  // result = result + bit * p;
+  // p *= 2;
+  // }
+
+  if (n.length() == 0) return 0;
+
   for (int i = n.size() - 1; i >= 0; i--) {
+    // get the last bit, but it's in the char, convert to int by - '0'
     int bit = n[i] - '0';
-    result = result + bit * p;
+    result += p * bit;
     p *= 2;
   }
 
@@ -58,8 +70,8 @@ int main() {
   ios_base::sync_with_stdio(0);
 
   cout << decToBi(10) << endl;
-  cout << biToDec(1000111) << endl;
-  cout << biStringToDec("1000111") << endl;
+  cout << biToDec(101) << endl;
+  cout << biStringToDec("101") << endl;
 
   return 0;
 }
