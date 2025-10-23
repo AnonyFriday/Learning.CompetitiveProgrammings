d353613560e74398e718b211f75839f104c9e6ee
anonyfriday <duykimvu75@gmail.com>
2025-10-23 09:57:55 +0700

    feat(bit): add bit manipulation operations

---
+++ Easy/6_Bitwise/00_CheckOdds.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+// #include <algorithm>
+// #include <cassert>
+// #include <climits>
+// #include <cmath>
+// #include <cstring>
+// #include <iostream>
+// #include <list>
+// #include <map>
+// #include <queue>
+// #include <set>
+// #include <stack>
+// #include <unordered_map>
+// #include <vector>
+//
+using namespace std;
+
+const double E = 1e-8;
+const double PI = acos(-1);
+
+/*
+1 0000 0001
+2 0000 0010
+3 0000 0011
+4 0000 0100
+5 0000 0101
+
+
+-> as you can see, the last digit is always 1 for odd, 0 or even
+-> so if we can extract the right most digit is 1, we can refer it as odd, vice versa
+
+*/
+
+bool isOdd(const int& a) {
+  return (a & 1) == 1;
+}
+
+int main() {
+  // dont use scanf and printf, only cin and cout
+  ios_base::sync_with_stdio(0);
+
+  // no more automatically calling cout.flush()
+  cin.tie(0);
+
+  int a = 1;
+
+  cout << isOdd(a);
+
+  return 0;
+}
