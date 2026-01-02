b6841ee0ed67ad3ac0e8c047aa7773738e8e3943
anonyfriday <duykimvu75@gmail.com>
2026-01-02 17:03:26 +0700

    chore: cleanup executables, add .gitignore

--- CSharp/4_Searching/Beginner/0_LinearSearch.cs
+++ CSharp/4_Searching/Beginner/0_LinearSearch.cs
@@ -1,6 +1,30 @@
-string line = Console.ReadLine();
+using System.Data;
 
-if (line != null)
+// ==================================
+// === CHANGE HERE
+// ==================================
+
+int LinearSearch(IList<int> list, int target)
 {
-    Console.WriteLine(line);
+    if (list == null || list.Count == 0) return -1;
+
+    for (int i = 0; i < list.Count; i++)
+    {
+        if (target == list[i]) return i;
+    }
+
+    return -1;
 }
+
+// ==================================
+// === MAIN EXECUTION
+// ==================================
+
+int.TryParse(Console.ReadLine(), out int no);
+int[] nums = Console.ReadLine().Split(" ").Select(e =>
+{
+    if (int.TryParse(e, out int num)) return num;
+    return 0;
+}).ToArray();
+
+Console.WriteLine(LinearSearch(nums, no));
