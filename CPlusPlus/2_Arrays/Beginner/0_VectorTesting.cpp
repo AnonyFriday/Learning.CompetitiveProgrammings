aad0cfa4d9ea36c012d4d2bd56bca64f475ccf92
anonyfriday <duykimvu75@gmail.com>
2026-01-03 14:36:20 +0700

    feat: add lower bound, sorted clostest pair sum on C# and C++

--- CPlusPlus/2_Arrays/Beginner/0_VectorTesting.cpp
+++ CPlusPlus/2_Arrays/Beginner/0_VectorTesting.cpp
@@ -9,16 +9,27 @@ int main() {
   vector<int> arr = {1, 2, 3, 4, 5, 6};
 
   // Fill Constructor
+  // - fill all 10 slots with value 7
   vector<int> arr1(10, 7);
 
+  // Vector are value type by default
+  // - Changing arr2, won't affect arr 1
+  vector<int> arr2 = arr1;
+  arr2[0] = 1222;
+
+  // Using pointer to affect the original array
+  vector<int> *arr3 = &arr1;
+  (*arr3)[0] = 1222;
+
+  // - fill all 100 slots with value 0
   vector<int> visited(100, 0);
 
   // Methods
   arr.pop_back();
   arr.push_back(16);
 
-  for (int i = 0; i < visited.size(); i++) {
-    cout << visited[i] << endl;
+  for (int i = 0; i < arr1.size(); i++) {
+    cout << arr1[i] << endl;
   }
 
   return 0;
