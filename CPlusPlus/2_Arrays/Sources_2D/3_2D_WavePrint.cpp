3fa8af434bb0c2c63c1116ee13ab921248c4f7e2
anonyfriday <duykimvu75@gmail.com>
2026-03-14 03:30:24 +0700

    chore: update folder for multidimensional array in C++

---
+++ CPlusPlus/2_Arrays/Sources_2D/3_2D_WavePrint.cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+
+// #include <algorithm>
+// #include <cassert>
+// #include <climits>
+// #include <cmath>
+// #include <cstring>
+// #include <iostream>
+// #include <list>
+// #include <map>
+// #include <queue>
+// #include <set>
+// #include <stack>
+// #include <unordered_map>
+// #include <vector>
+//
+using namespace std;
+
+const double E = 1e-8;
+const double PI = acos(-1);
+
+// Vectors
+template <typename T>
+void inputVector(vector<T>& arr);
+
+template <typename T>
+void outputVector(vector<T>& arr);
+
+template <typename T>
+void output2DVector(vector<vector<T>>& arr);
+
+template <typename T>
+void reverse(vector<T>& arr);
+
+// Maths
+int fact(int n);
+double calBinominalCoefficient(int n, int k);
+int reverse(int n);
+int largest(int largestNum, int target);
+
+// = DO HERE ========================
+
+vector<int> printWave(const vector<vector<int>>& arr) {
+  if (arr.empty() || arr.size() == 0) return {};
+
+  vector<int> result;
+  int rows = arr.size();
+  int cols = arr[0].size();
+  bool isDown = true;
+
+  for (int c = cols - 1; c >= 0; c--) {
+    if (isDown) {
+      for (int r = 0; r <= rows - 1; r++) {
+        result.push_back(arr[r][c]);
+      }
+    } else {
+      for (int r = rows - 1; r >= 0; r--) {
+        result.push_back(arr[r][c]);
+      }
+    }
+    isDown = !isDown;
+  }
+
+  return result;
+}
+
+// ==================================
+
+int main() {
+  // dont use scanf and printf, only cin and cout
+  ios_base::sync_with_stdio(0);
+
+  // no more automatically calling cout.flush()
+  cin.tie(0);
+
+  vector<vector<int>> arr = {
+      {1, 2, 3, 4},
+      {5, 6, 7, 8},
+      {9, 10, 11, 12},
+      {13, 14, 15, 16},
+  };
+
+  vector<int> wave = printWave(arr);
+
+  outputVector(wave);
+
+  return 0;
+}
+
+// Get the largestNumber
+int largest(int largestNum, int target) {
+  return largestNum < target ? target : largestNum;
+}
+
+// Input Vector Array
+template <typename T>
+void inputVector(vector<T>& arr) {
+  int i = 0;
+  while (i < arr.size()) {
+    cin >> arr[i];
+    i++;
+  }
+}
+
+// Output Vector Array
+template <typename T>
+void outputVector(vector<T>& arr) {
+  for (size_t i = 0; i < arr.size(); i++) {
+    std::cout << arr[i];
+
+    // print the last seperator
+    if (i != (arr.size() - 1)) {
+      std::cout << ",";
+    }
+  }
+
+  std::cout << endl;
+}
+
+// Ouput 2D vector array
+template <typename T>
+void output2DVector(vector<vector<T>>& arr) {
+  for (auto el : arr) {
+    for (size_t i = 0; i < el.size(); i++) {
+      std::cout << el[i];
+      if (i != (el.size() - 1)) {
+        std::cout << ",";
+      }
+    }
+    std::cout << endl;
+  }
+  std::cout << endl;
+}
+
+// Calculate factorial
+int fact(int n) {
+  if (n == 0 || n == 1) return 1;
+
+  return n * fact(n - 1);
+}
+
+// Calculate nCk
+// - counting(a,b) and (b,a) are the same value
+// - formular: n! / k!(n-k)!
+double calBinominalCoefficient(int n, int k) {
+  if (n == k) return 1.0;  // 1 way to choose everything
+  if (k == 1) return n;    // choose 1 item has N posibilities
+  if (k == 0) return 1.0;  // exactly 1 way to choose nothing
+
+  return (double)fact(n) / (double)(fact(k) * fact(n - k));
+}
+
+// Reverse a number
+int reverse(int n) {
+  if (n == 0 || n == 1) return n;
+
+  int res = 0;
+
+  while (n > 0) {
+    int digit = n % 10;
+    res = res * 10 + digit;
+    n /= 10;
+  }
+
+  return res;
+}
+
+// Reverse an array of T element
+template <typename T>
+void reverse(vector<T>& arr, int start, int end) {
+  if (arr.size() == 0 || start < 0 || end >= arr.size()) return;
+
+  while (start < end) {
+    std::swap(arr[start], arr[end]);
+    start++;
+    end--;
+  }
+}
