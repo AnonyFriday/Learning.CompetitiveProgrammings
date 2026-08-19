ae6e836d75752f238962970db1059b2d06e4290c
AnonyFriday <duykimvu75@gmail.com>
2026-08-19 21:32:47 +0700

    feat: add decorator

---
+++ Python/fundamentals/13_decorator.py
@@ -0,0 +1,32 @@
+# Takes in a function, adds some functionality and returns it 
+# Function wrap another function, add behavior without change original code.
+
+def make_pretty(input_func):
+    def decorate():
+        print("I got additional decoration.")
+        input_func()
+    return decorate
+
+def ordinary():
+    print("I am ordinary.")
+
+ordinary()
+pretty = make_pretty(ordinary)
+pretty()
+
+# Use it as the decorattor (in C#, it's Attribute)
+# syntatic sugar for say_hello = make_pretty(say_hello)
+# *args: unpack positional into tuple = params object[] args
+# **kwargs: foward whatever caller passed into
+def make_pretty_2(input_func):
+    def decorate(*args, **kwargs):
+        print("I got additional decoration.")
+        input_func(*args, **kwargs)
+    return decorate
+
+
+@make_pretty_2
+def say_hello(name, age):
+    print(f"Hello: {name} - {age}")
+
+say_hello("MONEY", 20)
